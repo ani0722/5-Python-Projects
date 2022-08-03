436abdee8e60ef2e6d493e8197bfe716166b0c53
Anish Kothari <107585475+ani0722@users.noreply.github.com>
2022-08-03 20:24:23 +0400

    Add files via upload
    Creating Batman logo with python turtle.

---
+++ Batman logo.py
@@ -0,0 +1,92 @@
+import turtle
+import math
+
+k= turtle.Turtle()
+k.speed(500)
+
+window = turtle.Screen()
+window.bgcolor("#000000")
+k.color("yellow")
+
+
+ankur = 20
+
+
+k.left(90)
+k.penup()
+k.goto(-7 * ankur, 0)
+k.pendown()
+
+
+for a in range(-7 * ankur, -3 * ankur, 1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = 1.5 * math.sqrt((-math.fabs(rel - 1)) * math.fabs(3 - rel) / ((rel - 1) * (3 - rel))) * (
+                1 + math.fabs(rel - 3) / (rel - 3)) * math.sqrt(1 - (x / 7) ** 2) + (
+                    4.5 + 0.75 * (math.fabs(x - 0.5) + math.fabs(x + 0.5)) - 2.75 * (
+                        math.fabs(x - 0.75) + math.fabs(x + 0.75))) * (1 + math.fabs(1 - rel) / (1 - rel))
+    k.goto(a, y * ankur)
+
+for a in range(-3 * ankur, -1 * ankur - 1, 1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = (2.71052 + 1.5 - 0.5 * rel - 1.35526 * math.sqrt(4 - (rel - 1) ** 2)) * math.sqrt(
+        math.fabs(rel - 1) / (rel - 1))
+    k.goto(a, y * ankur)
+
+k.goto(-1 * ankur, 3 * ankur)
+k.goto(int(-0.5 * ankur), int(2.2 * ankur))
+k.goto(int(0.5 * ankur), int(2.2 * ankur))
+k.goto(1 * ankur, 3 * ankur)
+print("Batman Logo")
+
+
+for a in range(1 * ankur + 1, 3 * ankur + 1, 1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = (2.71052 + 1.5 - 0.5 * rel - 1.35526 * math.sqrt(4 - (rel - 1) ** 2)) * math.sqrt(
+        math.fabs(rel - 1) / (rel - 1))
+    k.goto(a, y * ankur)
+
+for a in range(3 * ankur + 1, 7 * ankur + 1, 1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = 1.5 * math.sqrt((-math.fabs(rel - 1)) * math.fabs(3 - rel) / ((rel - 1) * (3 - rel))) * (
+                1 + math.fabs(rel - 3) / (rel - 3)) * math.sqrt(1 - (x / 7) ** 2) + (
+                    4.5 + 0.75 * (math.fabs(x - 0.5) + math.fabs(x + 0.5)) - 2.75 * (
+                        math.fabs(x - 0.75) + math.fabs(x + 0.75))) * (1 + math.fabs(1 - rel) / (1 - rel))
+    k.goto(a, y * ankur)
+
+
+
+for a in range(7 * ankur, 4 * ankur, -1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = (-3) * math.sqrt(1 - (x / 7) ** 2) * math.sqrt(math.fabs(rel - 4) / (rel - 4))
+    k.goto(a, y * ankur)
+
+    
+
+for a in range(4 * ankur, -4 * ankur, -1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = math.fabs(x / 2) - 0.0913722 * x ** 2 - 3 + math.sqrt(1 - (math.fabs(rel - 2) - 1) ** 2)
+    k.goto(a, y * ankur)
+
+    
+
+for a in range(-4 * ankur - 1, -7 * ankur - 1, -1):
+    x = a / ankur
+    rel = math.fabs(x)
+    y = (-3) * math.sqrt(1 - (x / 7) ** 2) * math.sqrt(math.fabs(rel - 4) / (rel - 4))
+    k.goto(a, y * ankur)
+
+
+
+k.penup()
+k.goto(300, 300)
+turtle.done()
+
+
+
+
